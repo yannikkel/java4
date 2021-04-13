098703dc9fc0ea6d84e95a2b86f043d3cc0e282b
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-04-13 11:50:12 +0500

    Delete BonusMilesService.java

--- BonusMilesService.java
+++
@@ -1,7 +0,0 @@
-public class BonusMilesService {
-    public int calculate(int cost){
-        int miles;
-                       miles = cost / 20;
-                return miles;
-            }
-        }
